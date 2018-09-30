1f67f1da063c7aced369ae4f6eca84c6bfcbc66d
Charlie Astrada <carlosastrada@gmail.com>
2018-09-30 14:26:44 -0300

    Refactor App to use jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,15 @@ class App extends React.Component {
   }
 
   render() {
-    return React.createElement('div', { id: 'root' }, [
-      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me'),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'havenese'
-      }),
-      React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'Bird',
-        breed: 'cockatiel'
-      }),
-      React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'Cat',
-        breed: 'mixed'
-      })
-    ]);
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havenese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
