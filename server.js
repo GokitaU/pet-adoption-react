29516a6f4e89fa0ffe76d487a61379c5335798b6
Charlie Astrada <carlosastrada@gmail.com>
2018-11-07 01:30:34 -0300

    Refactor node server to use renderToNodeStream

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToNodeStream } from 'react-dom/server';
 import { ServerLocation } from '@reach/router';
 import fs from 'fs';
 import App from './src/App.jsx';
@@ -14,14 +14,25 @@ const app = express();
 
 app.use('/dist', express.static('dist'));
 app.use((req, res) => {
+  res.write(parts[0]);
   const reactMarkup = (
     <ServerLocation url={req.url}>
       <App />
     </ServerLocation>
   );
 
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  //Create node stream
+  const stream = renderToNodeStream(reactMarkup);
+  //streams output to res
+  stream.pipe(
+    res,
+    { end: false }
+  );
+  //finish writing last part of rendered markup and end
+  stream.on('end', () => {
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 console.log(`listening on ${PORT}`);
